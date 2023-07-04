a812414574a8c8343c75b9a19fbc93840e0bd8cd
heba ibrahim <heba.ibrahim@itxi.net>
2023-07-04 13:13:36 +0300

    apply dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-angular-ssr-project';
+  isDarkMode = false;
+
+  ngOnInit(): void {
+    if (localStorage?.getItem('theme'))
+      this.isDarkMode = localStorage.getItem('theme') === 'dark';
+  }
+
+  onThemeClick() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
 }
